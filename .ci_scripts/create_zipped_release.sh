c12044775d92ce1b7f154a81fe9d6ad2c2213aa4
crudelios <crudelios@gmail.com>
2020-08-18 16:00:06 +0100

    Try fixing switch build

--- .ci_scripts/create_zipped_release.sh
+++ .ci_scripts/create_zipped_release.sh
@@ -4,6 +4,7 @@ build_dir="$(pwd)/build"
 
 if [ "$DEPLOY" = "switch" ]
 then
+chmod 777 $build_dir
 mkdir -p $build_dir/release/julius
 cp -f $build_dir/julius.nro $build_dir/release/julius/julius.nro
 cd $build_dir/release && zip -r ../julius_switch.zip julius -x "*.DS_Store" -x "*__MAC*"
