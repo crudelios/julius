9cd88de6ca9fc1ea4018e60b43e7725ff1c5e97d
José Cadete <crudelios@gmail.com>
2023-08-04 10:41:17 +0100

    Hopefully really add the mac app file now

--- .ci_scripts/build_upload.sh
+++ .ci_scripts/build_upload.sh
@@ -45,8 +45,8 @@ case "$DEPLOY" in
   ;;
 "mac")
   PACKAGE=mac
-  DEPLOY_FILE=augustus-$VERSION-mac.app
-  cp "${build_dir}/augustus.app" "deploy/$DEPLOY_FILE"
+  DEPLOY_FILE=augustus-$VERSION-mac.zip
+  cp "${build_dir}/augustus.zip" "deploy/$DEPLOY_FILE"
   ;;
 "android")
   PACKAGE=android
