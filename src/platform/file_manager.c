bb399e66dcb270db1136afa0a026c464db0617d0
José Cadete <crudelios@gmail.com>
2023-08-17 16:42:26 +0100

    Yet more logging

--- src/platform/file_manager.c
+++ src/platform/file_manager.c
@@ -277,12 +277,13 @@ int platform_file_manager_list_directory_contents(
         } else {
             char full_asset_path[FILE_NAME_MAX];
             if (assets_directory[assets_directory_length - 1] == '/' && dir[assets_directory_length] == '/') {
-                assets_directory_length++;
+             //   assets_directory_length++;
             }
             snprintf(full_asset_path, FILE_NAME_MAX, "%s%s", assets_directory, dir + assets_directory_length);
             current_dir = set_dir_name(full_asset_path);
             #ifndef BUILDING_ASSET_PACKER
-            SDL_Log("Asset dir: %s\n Internal dir: %s\nCurrent asset dir: %s\n", assets_directory, dir + assets_directory_length, current_dir);
+            SDL_Log("Last asset dir char: %c\n, First dir char: %c\nAsset dir: %s\n Internal dir: %s\nCurrent asset dir: %s\n",
+                assets_directory[assets_directory_length - 1], dir[assets_directory_length], assets_directory, dir + assets_directory_length, current_dir);
             #endif
         }
     } else {
